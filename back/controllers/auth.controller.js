c17ac3ccb86ec4e9e21eefd104b6dbb6187fac3d
LucasLenoir <95701721+LucasLenoir@users.noreply.github.com>
2022-03-22 15:54:48 +0100

    check&createUser backend

--- back/controllers/auth.controller.js
+++ back/controllers/auth.controller.js
@@ -6,16 +6,31 @@ module.exports.signUp = async (req, res) => {
   const { username, email, password } = req.body;
   console.log(req.body);
 
-  const insertUser = await sequelize.query(
-    "INSERT INTO users (username, email, password) VALUES(:data)",
+  const checkUser = await sequelize.query(
+    "SELECT * FROM users where username = :username or email = :email",
     {
       raw: true,
-      replacements: { data: [username, email, password] },
-      type: QueryTypes.INSERT,
+      replacements: { username: username, email: email },
+      type: QueryTypes.SELECT,
     }
   );
 
-  res.send(insertUser);
+  if (checkUser) {
+    console.log("user already exists");
+    console.log("email already exists");
+    res.status(400).send("ERROR, user or email already exists");
+  } else {
+    const insertUser = await sequelize.query(
+      "INSERT INTO users (username, email, password) VALUES(:data)",
+      {
+        raw: true,
+        replacements: { data: [username, email, password] },
+        type: QueryTypes.INSERT,
+      }
+    );
+
+    res.status(201).send(insertUser);
+  }
 };
 
 // module.exports.login = async (req, res) =>{
